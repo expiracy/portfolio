0bf9cd939f4f315f198b9d92a1eadb20bd8b400e
jgray <jameslgray.home@gmail.com>
2026-03-16 17:59:43 +0000

    replace amber theme with pink and purple theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
       <body className={firaCode.className}>
         <script
           dangerouslySetInnerHTML={{
-            __html: `(function(){try{var t=localStorage.getItem("theme");if(t&&["green","amber","blue","light"].includes(t)){document.documentElement.setAttribute("data-theme",t)}else{document.documentElement.setAttribute("data-theme","green")}}catch(e){document.documentElement.setAttribute("data-theme","green")}})()`,
+            __html: `(function(){try{var t=localStorage.getItem("theme");if(t&&["green","pink","blue","light"].includes(t)){document.documentElement.setAttribute("data-theme",t)}else{document.documentElement.setAttribute("data-theme","green")}}catch(e){document.documentElement.setAttribute("data-theme","green")}})()`,
           }}
         />
         <ThemeProvider>
